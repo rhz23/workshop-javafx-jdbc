ae9cba34da88c66298252e8d7d0529bb61f132a7
Rodrigo Zaninelli <rhzaninelli@gmail.com>
2021-07-01 19:12:53 -0300

    MainView Controller Implamentation

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,4 +1,41 @@
 package gui;
 
-public class MainViewController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainViewController implements Initializable {
+
+    @FXML
+    private MenuItem menuItemSeller;
+
+    @FXML
+    private MenuItem menuItemDepartment;
+
+    @FXML
+    private MenuItem menuItemAbout;
+
+    @FXML
+    private void onMenuItemSellerAction(){
+        System.out.println("onMenuItemSellerAction");
+    }
+
+    @FXML
+    private void onMenuItemDepartmentAction(){
+        System.out.println("onMenuItemDepartmentAction");
+    }
+
+    @FXML
+    private void onMenuItemAboutAction(){
+        System.out.println("onMenuItemAboutAction");
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
