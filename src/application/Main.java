c4b3d2f1710b7a2cdd4f6c1ca2387178c47a0558
Rodrigo Zaninelli <rhzaninelli@gmail.com>
2021-07-03 19:11:58 -0300

    implementing About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,26 +5,34 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Main extends Application {
 
+    private static Scene mainScene;
+
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage){
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
             ScrollPane scrollPane = loader.load();
+
             scrollPane.setFitToHeight(true); // comando para alinhar o scrollpane com a altura da janela
             scrollPane.setFitToWidth(true); //comando para alinhar o scrollpane com a largura da janela
-            //Scene mainScene = new Scene(parent);
-            primaryStage.setScene(new Scene(scrollPane));
+
+            mainScene = new Scene(scrollPane);
+            primaryStage.setScene(mainScene);
             primaryStage.setTitle("Sample JavaFX application");
             primaryStage.show();
-        } catch (IOException e) {
+            }catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    public static Scene getMainScene(){
+        return mainScene;
+    }
 }
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,9 +1,18 @@
 package gui;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -30,12 +39,38 @@ public class MainViewController implements Initializable {
 
     @FXML
     private void onMenuItemAboutAction(){
-        System.out.println("onMenuItemAboutAction");
+        loadView("/gui/About.fxml");
     }
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
 
+    private synchronized void loadView(String absoluteName){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+            VBox newVbox = loader.load();
+
+            Scene mainScene = Main.getMainScene();
+            VBox mainVbox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+            //getroot pega o primeiro elemento da mainScene
+            //(ScrollPane) faz o upcasting para ScrollPane (que é o tipo do painel utilizado na MainScene)
+            //getContent pega o conteudo dendo do ScrollPane da mainScene
+            //(VBox) faz o upcasting do que foi pega dentro do ScrollPane, que já é do tipo Vbox.
+            //toda essa informação é guardada dentro da variável "mainVbox"
+
+            Node mainMenu = mainVbox.getChildren().get(0);
+            //o comando acima pega o primeiro "children" dentro da mainVBox (filho da pasição 0)
+            mainVbox.getChildren().clear();
+            //o comando acima apaga todos os filhos que estiverem na mainVbox no momento que é chamado
+            mainVbox.getChildren().add(mainMenu);
+            //o comando acima adiciona o "mainMenu" ao mainVbox que estava limpo
+            mainVbox.getChildren().addAll(newVbox.getChildren());
+            //o comando acima adiciona todos os filhos do newVBox
+        }
+        catch(IOException e){
+            Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
+        }
     }
 }
