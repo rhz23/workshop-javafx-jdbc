b95b2c2a8bf3290007bd4230c1ff605b4594b625
Rodrigo Zaninelli <rhzaninelli@gmail.com>
2021-07-04 18:51:57 -0300

    Initializing Action as Parameter

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -16,6 +16,7 @@ import model.services.DepartmentService;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 public class MainViewController implements Initializable {
 
@@ -35,12 +36,15 @@ public class MainViewController implements Initializable {
 
     @FXML
     private void onMenuItemDepartmentAction(){
-        loadView2("/gui/DepartmentList.fxml");
+        loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
+            controller.setDepartmentService(new DepartmentService());
+            controller.updateTableView();
+        });
     }
 
     @FXML
     private void onMenuItemAboutAction(){
-        loadView("/gui/About.fxml");
+        loadView("/gui/About.fxml", x -> {});
     }
 
 
@@ -48,35 +52,8 @@ public class MainViewController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
     }
 
-    private synchronized void loadView(String absoluteName){
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-            VBox newVbox = loader.load();
-
-            Scene mainScene = Main.getMainScene();
-            VBox mainVbox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
-            //getroot pega o primeiro elemento da mainScene
-            //(ScrollPane) faz o upcasting para ScrollPane (que é o tipo do painel utilizado na MainScene)
-            //getContent pega o conteudo dendo do ScrollPane da mainScene
-            //(VBox) faz o upcasting do que foi pega dentro do ScrollPane, que já é do tipo Vbox.
-            //toda essa informação é guardada dentro da variável "mainVbox"
-
-            Node mainMenu = mainVbox.getChildren().get(0);
-            //o comando acima pega o primeiro "children" dentro da mainVBox (filho da pasição 0)
-            mainVbox.getChildren().clear();
-            //o comando acima apaga todos os filhos que estiverem na mainVbox no momento que é chamado
-            mainVbox.getChildren().add(mainMenu);
-            //o comando acima adiciona o "mainMenu" ao mainVbox que estava limpo
-            mainVbox.getChildren().addAll(newVbox.getChildren());
-            //o comando acima adiciona todos os filhos do newVBox
-        }
-        catch(IOException e){
-            Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
-        }
-    }
-
-
-    private synchronized void loadView2(String absoluteName){
+    //função genérica do tipo T (função parametrizada com tipo qualquer)
+    private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction){
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
             VBox newVbox = loader.load();
@@ -98,9 +75,9 @@ public class MainViewController implements Initializable {
             mainVbox.getChildren().addAll(newVbox.getChildren());
             //o comando acima adiciona todos os filhos do newVBox
 
-            DepartmentListController controller = loader.getController();
-            controller.setDepartmentService(new DepartmentService());
-            controller.updateTableView();
+            //comandos para aceitar/injetar os comandos/parametros inicializados com a chamada do loadView
+            T controller = loader.getController();
+            initializingAction.accept(controller);
         }
         catch(IOException e){
             Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
