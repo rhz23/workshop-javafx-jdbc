b51cfd2fc4df86e07b2d215c8157f97855b7dfc7
Rodrigo Zaninelli <rhzaninelli@gmail.com>
2021-07-04 18:32:19 -0300

    DepartmentService Implementation and mockdata tests

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,6 +1,8 @@
 package gui;
 
 import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -10,11 +12,16 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class DepartmentListController implements Initializable {
+
+    //Dependencia da classe DepartmentService (criada a variavel service do tipo DepartmentService)
+    private DepartmentService service;
 
     @FXML
     private TableView<Department> tableViewDepartment;
@@ -28,11 +35,17 @@ public class DepartmentListController implements Initializable {
     @FXML
     private Button btNew;
 
+    private ObservableList<Department> obsList;
+
     @FXML
     public void onBtNewAction(){
         System.out.println("onBtNewAction");
     }
 
+    //seta a variavel service com o DepartmentService que foi criado em outro lugar
+    public void setDepartmentService(DepartmentService service){
+        this.service = service;
+    }
 
 
     @Override
@@ -49,5 +62,18 @@ public class DepartmentListController implements Initializable {
 
         Stage stage = (Stage) Main.getMainScene().getWindow();
         tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
+    }
+
+    //método responsavel por atualizar a ObservableList obsList
+    public void updateTableView(){
+        //verificar se o service foi carregado corretamente
+        if(service == null){
+            throw new IllegalStateException("Service was null");
+        }
+        List<Department> list = service.findAll();
+        obsList = FXCollections.observableArrayList(list);
+        //o comando acima instancia a obsList pegando os dados da list acima
+        //carrega os elementos da obsList na tableViewDepartment
+        tableViewDepartment.setItems(obsList);
     }
 }
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -11,6 +11,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import model.services.DepartmentService;
 
 import java.io.IOException;
 import java.net.URL;
@@ -34,7 +35,7 @@ public class MainViewController implements Initializable {
 
     @FXML
     private void onMenuItemDepartmentAction(){
-        loadView("/gui/DepartmentList.fxml");
+        loadView2("/gui/DepartmentList.fxml");
     }
 
     @FXML
@@ -68,6 +69,38 @@ public class MainViewController implements Initializable {
             //o comando acima adiciona o "mainMenu" ao mainVbox que estava limpo
             mainVbox.getChildren().addAll(newVbox.getChildren());
             //o comando acima adiciona todos os filhos do newVBox
+        }
+        catch(IOException e){
+            Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
+        }
+    }
+
+
+    private synchronized void loadView2(String absoluteName){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+            VBox newVbox = loader.load();
+
+            Scene mainScene = Main.getMainScene();
+            VBox mainVbox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+            //getroot pega o primeiro elemento da mainScene
+            //(ScrollPane) faz o upcasting para ScrollPane (que é o tipo do painel utilizado na MainScene)
+            //getContent pega o conteudo dendo do ScrollPane da mainScene
+            //(VBox) faz o upcasting do que foi pega dentro do ScrollPane, que já é do tipo Vbox.
+            //toda essa informação é guardada dentro da variável "mainVbox"
+
+            Node mainMenu = mainVbox.getChildren().get(0);
+            //o comando acima pega o primeiro "children" dentro da mainVBox (filho da pasição 0)
+            mainVbox.getChildren().clear();
+            //o comando acima apaga todos os filhos que estiverem na mainVbox no momento que é chamado
+            mainVbox.getChildren().add(mainMenu);
+            //o comando acima adiciona o "mainMenu" ao mainVbox que estava limpo
+            mainVbox.getChildren().addAll(newVbox.getChildren());
+            //o comando acima adiciona todos os filhos do newVBox
+
+            DepartmentListController controller = loader.getController();
+            controller.setDepartmentService(new DepartmentService());
+            controller.updateTableView();
         }
         catch(IOException e){
             Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
