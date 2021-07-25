9a78c5527c016d50a419b4789d193f7e119cadb1
Rodrigo Zaninelli <rhzaninelli@gmail.com>
2021-07-25 15:54:48 -0300

    Validation Exception

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -13,12 +13,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exceptions.ValidationException;
 import model.services.DepartmentService;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class DepartmentFormController implements Initializable {
 
@@ -69,6 +68,9 @@ public class DepartmentFormController implements Initializable {
             notifyDataSetChangeListeners();
             Utils.currentStage(event).close(); //comando para fechar a janela, necessário colocar o "event" como paramtro de entrada do método
             }
+        catch (ValidationException e){
+            setErrorMessages(e.getErrors());
+        }
         catch (DbException e){
             Alerts.showAlert("Error saving object", null, e.getMessage(), Alert.AlertType.ERROR);
         }
@@ -83,8 +85,19 @@ public class DepartmentFormController implements Initializable {
 
     private Department getFormData() {
         Department obj = new Department();
+
+        ValidationException exception = new ValidationException("Validation Exception");
+
         obj.setId(Utils.tryParseToInt(txtId.getText()));
+
+        if (txtName.getText() == null || txtName.getText().trim().equals("")){
+            exception.addErrors("name", "Field can´t be empty");
+        }
         obj.setName(txtName.getText());
+
+        if(exception.getErrors().size() > 0){
+            throw exception;
+        }
 
         return obj;
     }
@@ -111,5 +124,13 @@ public class DepartmentFormController implements Initializable {
         }
         txtId.setText(String.valueOf(entity.getId())); //método para popular o campo Id do formulario com o valor do Id do departamento
         txtName.setText(entity.getName());
+    }
+
+    private void setErrorMessages(Map<String, String> errors){
+        Set<String> fields  = errors.keySet();
+
+        if(fields.contains("name")){
+            labelErrorName.setText(errors.get("name"));
+        }
     }
 }
